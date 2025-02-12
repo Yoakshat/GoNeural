0030b8a1ae3367a073063240f4b87452bbc88ef1
Yoakshat <51726869+Yoakshat@users.noreply.github.com>
2025-02-11 23:26:36 -0800

    Collected aloss/ainput gradients
    Added loss function. Reimplemented collecting gradients backwards. Specifically aloss/ainput gradients. Now, no errors!

--- main.go
+++ main.go
@@ -9,13 +9,24 @@ func main() {
 	input := [][]float64{{1, 2, -1, 0}}
 	w1 := [][]float64{{1, 0, 1}, {0, -1, 0},
 		{1, -1, 0}, {1, 1, 1}}
+	// 3 columns, 2 rows
+	w2 := [][]float64{{3, 1}, {2, 0}, {1, 2}}
 
-	layer := []nn.Operation{&nn.Linear{Cols: 4, Rows: 3, Mtx: w1}, &nn.Relu{}}
+	// in theory, as many linears and relus in fashion should work
+	// to try out tmrw!
+
+	layer := []nn.Operation{
+		&nn.Linear{Cols: 4, Rows: 3, Mtx: w1},
+		&nn.Relu{},
+		&nn.Linear{Cols: 3, Rows: 2, Mtx: w2},
+		&nn.Loss{},
+	}
 	feedforward := nn.Network{Ops: layer}
 	res := feedforward.Perform(input)
 
 	fmt.Println(res)
 
+	// facing errors with our updaate
 	feedforward.Update(1e-3)
 
 	// fmt.Println(input)
--- nn/network.go
+++ nn/network.go
@@ -8,7 +8,15 @@ import (
 type Operation interface {
 	Perform(mx [][]float64) [][]float64
 	UpdateWeight(grad [][]float64, lr float64)
+
+	// first we compute grad to be
+	// gradient of the next layer's neurons over this neuron
+
+	// then we work backwards to mean aloss/ainput
 	GetGrad() [][]float64
+	SetGrad([][]float64)
+
+	isActivation() bool
 }
 
 // gradient: alpha function after / alpha this
@@ -26,10 +34,50 @@ type Relu struct {
 	Grad [][]float64
 }
 
+type Loss struct {
+	Grad [][]float64
+}
+
 // a network is just a list of operations
 type Network struct {
 	// list of operations
 	Ops []Operation
+}
+
+// performing our loss layer
+func (l *Loss) Perform(mx [][]float64) [][]float64 {
+	// return 1 number as matrix
+	var lossMtx [][]float64 = [][]float64{{0}}
+	var grad [][]float64
+
+	vec := mx[0]
+
+	for i := 0; i < len(vec); i++ {
+		// for now -> our loss
+		lossMtx[0][0] += vec[i] * vec[i]
+
+		// append to l.Grad 2 * vec[i]
+		grad = append(grad, []float64{2 * vec[i]})
+	}
+
+	l.Grad = grad
+	return lossMtx
+}
+
+func (l Loss) UpdateWeight(grad [][]float64, lr float64) {
+	// no need to update anything
+}
+
+func (l Loss) GetGrad() [][]float64 {
+	return l.Grad
+}
+
+func (l *Loss) SetGrad(grad [][]float64) {
+	l.Grad = grad
+}
+
+func (l Loss) isActivation() bool {
+	return false
 }
 
 func (r *Relu) Perform(mx [][]float64) [][]float64 {
@@ -42,15 +90,22 @@ func (r Relu) UpdateWeight(grad [][]float64, lr float64) {
 	// no need to update anything
 }
 
+func (r *Relu) SetGrad(grad [][]float64) {
+	r.Grad = grad
+}
+
 func (r Relu) GetGrad() [][]float64 {
 	return r.Grad
 }
 
+func (r Relu) isActivation() bool {
+	return true
+}
+
 func (l *Linear) Perform(mx [][]float64) [][]float64 {
 
-	l.Grad = mx
-	// matrix.RepeatRow(matrix.Transpose(mx), l.Rows)
-
+	// gradient is weights
+	l.Grad = l.Mtx
 	var res, _ = matrix.MatrixMult(l.Mtx, mx)
 
 	return res
@@ -68,6 +123,14 @@ func (l Linear) GetGrad() [][]float64 {
 	return l.Grad
 }
 
+func (l *Linear) SetGrad(grad [][]float64) {
+	l.Grad = grad
+}
+
+func (l Linear) isActivation() bool {
+	return false
+}
+
 func (n Network) Perform(mx [][]float64) [][]float64 {
 
 	for _, op := range n.Ops {
@@ -78,25 +141,54 @@ func (n Network) Perform(mx [][]float64) [][]float64 {
 }
 
 // update weights
+// assumes always linear before activation
 func (n Network) Update(lr float64) {
-	var gradUpdate [][]float64
+	// first step is to go backwards
+	// and collect aloss/ainput gradients
+	o := len(n.Ops)
+	gradUpdate := n.Ops[o-1].GetGrad()
+	o -= 1
+	condition := true
 
-	// go backwards
-	for o := len(n.Ops); o > 0; o-- {
-		// o - 1
-		op := n.Ops[o-1]
+	for condition {
+		fmt.Println(o)
 
-		// last operation
-		if o == len(n.Ops) {
-			gradUpdate = op.GetGrad()
-		} else {
-			// gradUpdate * transpose of new op's gradient
-			gradUpdate, _ = matrix.MatrixMult(gradUpdate, matrix.Transpose(op.GetGrad()))
-			fmt.Println(gradUpdate)
-
+		if o < 1 {
+			break
 		}
-		// if weights layer actually update it
-		op.UpdateWeight(gradUpdate, lr)
+
+		// first check if isActivation
+		newOp := n.Ops[o-1]
+		var newGrad [][]float64
+
+		newGrad = newOp.GetGrad()
+
+		// but if activation
+		if newOp.isActivation() {
+			temp := newOp.GetGrad()
+
+			o -= 1
+			newOp = n.Ops[o-1]
+
+			// elementwise multiplication
+			// with 0's and 1's in ReLU
+			newGrad = matrix.MultE(temp, newOp.GetGrad())
+		}
+
+		// then carry on how you were before
+
+		// [[6]]
+		fmt.Println(gradUpdate)
+		// [[0 0 1]]
+
+		// WE FORGOT ABOUT THE 2 inputs (ouch)
+		fmt.Println(newGrad)
+		gradUpdate, _ = matrix.MatrixMult(gradUpdate, newGrad)
+		newOp.SetGrad(gradUpdate)
+
+		o -= 1
 	}
+
+	// second step is to go and actually update the weights
 
 }
